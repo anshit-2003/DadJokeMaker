a9865ffc968c06509b40101ef2b1db441d291530
anshit-2003 <anshitbakshi@Anshits-MacBook-Air.local>
2023-04-25 23:16:37 +0530

    Adding Support For Hindi Jokes as well

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const getjoke = async() =>{
+const h2 = document.querySelector('#joke');
+const language = document.querySelector("#language");
+const button = document.querySelector('#Generate');
+const getjokeeng = async() =>{
     const config = {headers: {Accept : "application/json"}}
     const res = await axios.get('https://icanhazdadjoke.com',config);
     return res.data.joke
 }
-const h2 = document.querySelector('#joke');
-const button = document.querySelector('#Generate');
-const addnewjoke = async() =>{
-    const joke = await getjoke();
+
+const getjokehindi = async () =>{
+    const res = await axios.get('https://hindi-jokes-api.onrender.com/jokes?api_key=730345873dcf3f6eb674014cc9af');
+    return res.data.jokeContent;
+
+}
+const addnewjoke = async(getjokeeng) =>{
+    const joke = await getjokeeng();
     h2.innerText = joke;
 
 }
-button.addEventListener('click', addnewjoke)
+
+button.addEventListener('click', () => {
+    if(language.value == "Hindi"){
+        addnewjoke(getjokehindi);
+    }
+    else if(language.value == "English"){
+        addnewjoke(getjokeeng);
+    }
+})
