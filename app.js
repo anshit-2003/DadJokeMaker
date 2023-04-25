570cee004efbca979532fde7132c299fbb168a99
anshit-2003 <anshitbakshi@Anshits-MacBook-Air.local>
2023-04-25 18:20:48 +0530

    Adding JavaScript Logic

---
+++ app.js
@@ -0,0 +1,13 @@
+const getjoke = async() =>{
+    const config = {headers: {Accept : "application/json"}}
+    const res = await axios.get('http://icanhazdadjoke.com',config);
+    return res.data.joke
+}
+const h2 = document.querySelector('#joke');
+const button = document.querySelector('#Generate');
+const addnewjoke = async() =>{
+    const joke = await getjoke();
+    h2.innerText = joke;
+
+}
+button.addEventListener('click', addnewjoke)
